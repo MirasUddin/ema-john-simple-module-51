c869eb9dada34b95869eedc295931e7b81e5b43f
MirasUddin <mirasuddin978@gmail.com>
2023-03-28 17:38:23 +0600

    retrieve product from stored cart and get it to cart steate

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { addToDb, getShoppingCart } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
@@ -14,17 +15,54 @@ const Shop = () => {
             .then(data => setProducts(data))
     }, []);
 
+
+
+    useEffect(()=>{
+        const storedCart = getShoppingCart();
+        const saveCart = [];
+        // step 1 : get the id of the added product 
+        for(const id in storedCart){
+            //steps 2: get product from products state by using id
+            const addedProduct = products.find(product => product.id === id)
+            if(addedProduct){
+                // steps 3: and quantity 
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                // step 4: add to the added product to the saved cart
+                saveCart.push(addedProduct);
+            }
+            console.log(addedProduct);
+        }
+        // step 5: set the cart
+        setCart(saveCart)
+    },[products])
+    
+    
+    
     const handleAddToCart = (product) => {
         // cart.push(product); 
         const newCart = [...cart, product];
         setCart(newCart);
+        addToDb(product.id)
     }
 
-    // useEffect(() => {
-    //     fetch('products.json')
-    //         .then(res => res.json())
-    //         .then(data => setCart(data))
-    // }, [])
+
+ 
+    
+    
+    // useEffect( ()=>{
+    //     // console.log('product', products);
+    //     const addedProduct = getShoppingCart();
+    //     // step 1: get id
+    //     for(const id in addedProduct){
+    //         // step 2: get the product by using id
+    //         const savedProduct = products.find(product => product.id === id)
+    //         // get the quantity of the product
+    //         const quantity = addedProduct[id]
+    //         addedProduct.quantity = quantity;
+    //         console.log(savedProduct);
+    //     }
+    // },[products])
 
     return (
         <div className='shop-container'>
